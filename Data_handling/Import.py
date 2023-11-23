65068a6c6ab09052a25cd6a93757a82b00b9e572
Thijs <drosttj@xs4all.nl>
2023-11-23 11:39:09 +0100

    Add calibration fitting

--- Data_handling/Import.py
+++ Data_handling/Import.py
@@ -4,6 +4,8 @@ import warnings
 
 import numpy as np
 import h5py
+import scipy
+import lmfit
 
 
 def import_hdf5(loc, dependent):
@@ -181,4 +183,48 @@ class DataSet(SimpleDataSet):
                            self.variable_name, self.wavelength_range, self.baseline_correction, self._selected_num)
         else:
             raise TypeError(f"Can't add {type(other)} to DataSet")
+
+
+class InterpolationCalibrationSet(SimpleDataSet):
+    def __init__(self, wavelength, absorbances, variable, variable_name, add_zero=True):
+        if add_zero:
+            absorbances = np.concatenate((np.zeros((1, absorbances.shape[1])), absorbances))
+            variable = np.concatenate((np.zeros(1), variable))
+        if len(np.unique(variable)) != len(variable):
+            raise ValueError('Variable values must be unique')
+        sort_index = np.argsort(variable)
+        super().__init__(wavelength, absorbances[sort_index], variable[sort_index], np.zeros(len(variable)), variable_name)
+        self._interpolator = scipy.interpolate.interp1d(variable, absorbances, axis=0, kind='linear')
+
+    @staticmethod
+    def from_simple(simple_data_set, add_zero=True):
+        return InterpolationCalibrationSet(simple_data_set.wavelength, simple_data_set.absorbances, simple_data_set.variable,
+                                           simple_data_set.variable_name, add_zero)
+
+    def interpolate(self, variable_value):
+        if variable_value < np.min(self.variable) or variable_value > np.max(self.variable):
+            raise ValueError('Variable value out of range')
+        if variable_value in self.variable:
+            return self[self.variable == variable_value]
+        else:
+            return self._interpolate(variable_value)
+
+    def _interpolate(self, variable_value):
+        return self._interpolator(variable_value)
+
+    def fit(self, absorbance):
+        def residual(params, absorbance):
+            variable = params[self.variable_name]
+            return self.interpolate(variable) - absorbance
+
+        params = lmfit.Parameters()
+        params.add(self.variable_name, value=np.mean(self.variable), min=np.min(self.variable), max=np.max(self.variable))
+        result = lmfit.minimize(residual, params, kws={'absorbance': absorbance})
+        return result
+
+    def model(self):
+        interp_model = lmfit.Model(self.interpolate)
+        interp_model.set_param_hint(self.variable_name, min=np.min(self.variable), max=np.max(self.variable), vary=True,
+                                    value=np.mean(self.variable))
+        return interp_model
 
