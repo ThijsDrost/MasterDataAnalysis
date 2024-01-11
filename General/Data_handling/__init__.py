468303e15573d8a07f24694ceed279febd9ba41d
Thijs <drosttj@xs4all.nl>
2024-01-11 18:09:36 +0100

    Update project structure

--- General/Data_handling/__init__.py
+++ General/Data_handling/__init__.py
@@ -1,3 +1,3 @@
-from .Import import drive_letter, import_hdf5, SimpleDataSet, InterpolationSet
+from .Import import drive_letter, import_hdf5, SimpleDataSet, InterpolationDataSet, DataSet
 from .Conversion import make_hdf5
 from .Data_import import SpectroData
