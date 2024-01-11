d3b80b8b4d8f382fcf9479e2cbaca4e0dbb12784
Thijs <drosttj@xs4all.nl>
2024-01-11 18:10:08 +0100

    Stub

---
+++ General/Analysis/Fit.py
@@ -0,0 +1,9 @@
+from General.Analysis import Analyzer, Models
+from General.Data_handling import drive_letter, InterpolationDataSet, import_hdf5
+
+class LinesModel:
+    def __init__(self, ranges, corrected=True, num=1, **kwargs):
+        self.ranges = ranges
+        self.corrected = corrected
+        self.num = num
+        self.kwargs = kwargs
