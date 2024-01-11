de18c097adf403fbe7dee77a476f085dbb2607c5
Thijs <drosttj@xs4all.nl>
2024-01-11 12:21:44 +0100

    Rename Analysis.py to Analyzer.py

--- General/Data_handling/__init__.py
+++ General/Data_handling/__init__.py
@@ -1,4 +1,4 @@
-from .Analysis import Analyzer
+from .Analyzer import Analyzer
 from .Import import drive_letter, import_hdf5, SimpleDataSet, InterpolationSet
 from .Conversion import make_hdf5
 from .Data_import import SpectroData
